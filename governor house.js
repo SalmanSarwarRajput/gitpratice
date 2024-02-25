6ab8438ac2dc08d2739a756753ad0400c2df7218
salman <sarwarsalman92@gmail.com>
2024-02-25 12:21:53 +0500

    first commit

---
+++ governor house.js
@@ -0,0 +1,6 @@
+var progam = "Governor House";
+console.log(progam);
+var program = 'Kamran Khan Tessori';
+console.log(program);
+var username = "Sir Ameen Alam";
+console.log(username);
---
+++ governor house.ts
@@ -0,0 +1,6 @@
+let progam="Governor House"
+console.log(progam);
+let program='Kamran Khan Tessori'
+console.log(program)
+let username: string="Sir Ameen Alam"
+console.log(username)
